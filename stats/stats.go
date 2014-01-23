27a8d02c6ae8284d3283ff312144555ccb18c918
Michael Crosby <michael@crosbymichael.com>
2014-01-23 08:49:23 -0800

    Move stats flag to pkg and add influxdb support

--- stats/stats.go
+++ stats/stats.go
@@ -1,7 +1,13 @@
 package stats
 
 import (
+	"flag"
 	"github.com/rcrowley/go-metrics"
+	"github.com/rcrowley/go-metrics/influxdb"
+	"github.com/rcrowley/go-metrics/stathat"
+	"log"
+	"net"
+	"os"
 )
 
 var (
@@ -11,9 +17,23 @@ var (
 	UpdateTTLCount     metrics.Counter
 	GetServiceCount    metrics.Counter
 	RemoveServiceCount metrics.Counter
+
+	metricsToStdErr             bool
+	graphiteServer, stathatUser string
+	config                      *influxdb.Config
 )
 
 func init() {
+	config = &influxdb.Config{}
+
+	flag.BoolVar(&metricsToStdErr, "metricsToStdErr", false, "Write metrics to stderr periodically")
+	flag.StringVar(&graphiteServer, "graphiteServer", "", "Graphite Server connection string e.g. 127.0.0.1:2003")
+	flag.StringVar(&stathatUser, "stathatUser", "", "StatHat account for metrics")
+	flag.StringVar(&config.Host, "influxHost", "", "Influxdb host for metrics")
+	flag.StringVar(&config.Database, "influxDatabase", "", "Influxdb database for metrics")
+	flag.StringVar(&config.Username, "influxUsername", "", "Influxdb username for metrics")
+	flag.StringVar(&config.Password, "influxPassword", "", "Influxdb password for metrics")
+
 	ExpiredCount = metrics.NewCounter()
 	metrics.Register("skydns-expired-entries", ExpiredCount)
 
@@ -31,4 +51,28 @@ func init() {
 
 	RemoveServiceCount = metrics.NewCounter()
 	metrics.Register("skydns-remove-service-requests", RemoveServiceCount)
+}
+
+// StartCollection begins the user defined metric
+// collection to the specified services in the background
+func StartCollection() {
+	// Set up metrics if specified on the command line
+	if metricsToStdErr {
+		go metrics.Log(metrics.DefaultRegistry, 60e9, log.New(os.Stderr, "metrics: ", log.Lmicroseconds))
+	}
+
+	if graphiteServer != "" {
+		addr, err := net.ResolveTCPAddr("tcp", graphiteServer)
+		if err == nil {
+			go metrics.Graphite(metrics.DefaultRegistry, 10e9, "skydns", addr)
+		}
+	}
+
+	if stathatUser != "" {
+		go stathat.Stathat(metrics.DefaultRegistry, 10e9, stathatUser)
+	}
+
+	if config.Host != "" {
+		go influxdb.Influxdb(metrics.DefaultRegistry, 10e9, config)
+	}
 }
